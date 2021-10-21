a7a1b1566afdab9bcec4bdbd2511504c4798b87d
Nick Brimmer <nickbrimmer@gmail.com>
2021-10-20 19:27:25 -0700

    creating first blog structure

---
+++ pages/blog/index.js
@@ -0,0 +1,7 @@
+// INDEX.JS WILL ROUTE TO THE ROOT OF THE FOLDER
+
+function Blog(){
+    return <h1>Blog Page</h1>
+}
+
+export default Blog
---
+++ pages/blog/second.js
@@ -0,0 +1,5 @@
+export const second = () => {
+    return <h1>This is my SECOND blog</h1>
+}
+
+export default second 
